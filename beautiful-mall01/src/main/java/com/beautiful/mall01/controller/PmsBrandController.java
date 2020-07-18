a639d73e15fb3b527708b451cc23dab7f74a5367
yongjiu.dang <yongjiu.dang@foxmail.com>
2020-07-18 17:53:55 +0800

    添加Swagger2 生成在线接口文档

--- beautiful-mall01/src/main/java/com/beautiful/mall01/controller/PmsBrandController.java
+++ beautiful-mall01/src/main/java/com/beautiful/mall01/controller/PmsBrandController.java
@@ -5,6 +5,8 @@ import com.beautiful.mall01.common.CommonResult;
 import com.beautiful.mall01.common.ResultCode;
 import com.beautiful.mall01.mbg.model.PmsBrand;
 import com.beautiful.mall01.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.List;
  * @Date 2020/7/17 19:15
  * @Version 1.0
  */
+@Api(tags = "PmsBrandController",description = "商品品牌管理")
 @RestController
 @RequestMapping("/brand")
 @Slf4j
@@ -24,17 +27,20 @@ public class PmsBrandController {
     @Autowired
     PmsBrandService pmsBrandService;
 
+    @ApiOperation("查询所有品牌列表")
     @GetMapping("/listAll")
     public CommonResult<List<PmsBrand>> listAllBrand(){
         return CommonResult.success(pmsBrandService.listAllBrand());
     }
 
+    @ApiOperation("按照页数查询品牌列表")
     @GetMapping("/list")
     public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam("pageNum") int pageNum,@RequestParam("pageSize") int pageSize){
         List<PmsBrand> list=pmsBrandService.listBrand(pageNum,pageSize);
         return CommonResult.success(CommonPage.restPage(list));
     }
 
+    @ApiOperation("添加品牌")
     @PostMapping("/create")
     public CommonResult<PmsBrand> createBrand(@RequestBody PmsBrand pmsBrand){
         int code=pmsBrandService.createBrand(pmsBrand);
@@ -47,6 +53,7 @@ public class PmsBrandController {
         }
     }
 
+    @ApiOperation("更新品牌")
     @PostMapping("/update/{id}")
     public CommonResult<PmsBrand> updateBrand(@PathVariable("id") Long id,@RequestBody PmsBrand pmsBrand){
         int code=pmsBrandService.updateBrand(id,pmsBrand);
@@ -59,6 +66,7 @@ public class PmsBrandController {
         }
     }
 
+    @ApiOperation("根据ID删除品牌")
     @DeleteMapping("/delete/{id}")
     public CommonResult deleteBrand(@PathVariable("id") Long id){
         int code = pmsBrandService.deleteBrand(id);
@@ -71,6 +79,7 @@ public class PmsBrandController {
         }
     }
 
+    @ApiOperation("根据ID查询品牌")
     @GetMapping("/getBrand/{id}")
     public CommonResult<PmsBrand> getBrand(@PathVariable("id") Long id){
         PmsBrand brand = pmsBrandService.getBrand(id);
